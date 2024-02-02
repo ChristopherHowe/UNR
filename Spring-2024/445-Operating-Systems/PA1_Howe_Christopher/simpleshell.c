8d7cc61eb0ebe1c9a35dcd78e7e173b7a38bb515
ChristopherHowe <howechristopher5@gmail.com>
2024-02-01 22:30:25 -0800

    improved error handling

--- Spring-2024/445-Operating-Systems/PA1_Howe_Christopher/simpleshell.c
+++ Spring-2024/445-Operating-Systems/PA1_Howe_Christopher/simpleshell.c
@@ -9,6 +9,10 @@ Title: CS 446 PA1
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
 
 
 // The following libraries can be used
@@ -25,109 +29,87 @@ Title: CS 446 PA1
 // exit, cd, all commands supported by execvp
 // execvp supports ls -l and clear
 
-#define MAX_ARGS 20
-#define MAX_ARG_LEN 20
+#define MAX_TOKENS 20
+#define MAX_TOKEN_LEN 20
 #define MAX_CWD_LEN 200
 #define NET_ID "christopherhowe"
 
-int parseInput(char* input, char* command, char args[MAX_ARGS][MAX_ARG_LEN]);
-void printArgs(char args[MAX_ARGS][MAX_ARG_LEN], int numArgs);
+int parseInput(char* input, char args[MAX_TOKENS][MAX_TOKEN_LEN]);
+void printArgs(char args[MAX_TOKENS][MAX_TOKEN_LEN], int numArgs);
 void writePrompt();
 void changeDirectories(char* path);
-void executeCommand(char* command, char args [MAX_ARGS][MAX_ARG_LEN], int numArgs);
-
+void executeCommand(char args [MAX_TOKENS][MAX_TOKEN_LEN], int numArgs);
+void wrapError(const char* msg, char* err);
 
 int main(){
     while(1){
         writePrompt();
-        char args[MAX_ARGS][MAX_ARG_LEN];
         char input[100];
-        char command[20];
+       
         scanf(" %[^\n]", input);
         printf("read input to be %s\n", input);
-        int numArgs = parseInput(input, command, args);
-        printf("Parsed Command: %s\n", command);
-        printArgs(args, numArgs);
-        if (strcmp(command,"cd") == 0){
-            if (numArgs >= 2){
-                printf("SimpleShell: cd: too many arguments\n");
-                continue;
-            }
-            changeDirectories(args[0]);
-        } else if (strcmp(command,"exit") == 0){
+        
+        char commandAndArgs[MAX_TOKENS][MAX_TOKEN_LEN];
+        int numArgs = parseInput(input, commandAndArgs);
+        printf("Parsed Command: %s\n", commandAndArgs[0]);
+        printArgs(commandAndArgs, numArgs + 1);
+        if (strcmp(commandAndArgs[0],"cd") == 0){
+            changeDirectories(commandAndArgs[1]);
+        } else if (strcmp(commandAndArgs[0],"exit") == 0){
             break;
         } 
         else {
             printf("input: %s\n",input);
-            executeCommand(command, args, numArgs);
+            executeCommand(commandAndArgs, numArgs);
         }
     }
     return 0;
 }
-
+void wrapError(const char* msg, char* err){
+    printf("SimpleShell: %s %s\n", msg, err);
+}
 void writePrompt(){
     char cwd[MAX_CWD_LEN];
     getcwd(cwd, MAX_CWD_LEN);
     printf("%s:%s$ ", NET_ID, cwd);
 }
 
-int parseInput(char* input, char* command, char args[MAX_ARGS][MAX_ARG_LEN]){
-    char* token = strtok(input, " ");
-    strcpy(command, token);
+int parseInput(char* input, char args[MAX_TOKENS][MAX_TOKEN_LEN]){
     int numArgs = 0;
+    char* token = strtok(input, " ");
+    strcpy(args[0], token); // command
     token = strtok(NULL, " ");
     while (token != NULL){
-        strcpy(args[numArgs], token);
+        strcpy(args[numArgs + 1], token);
         token = strtok(NULL, " ");
         numArgs++;
     }
     return numArgs;
 }
 // TO DELETE
-void printArrayOfStrings(char strings[MAX_ARGS][MAX_ARG_LEN], int numStrings) {
+void printArrayOfStrings(char strings[MAX_TOKENS][MAX_TOKEN_LEN], int numStrings) {
     for (int i = 0; i < numStrings; i++) {
         printf("%s, ", strings[i]);
     }
 }
 // TO DETELE
-void printArgs(char args[MAX_ARGS][MAX_ARG_LEN], int numArgs){
+void printArgs(char args[MAX_TOKENS][MAX_TOKEN_LEN], int numArgs){
     printf("Args: ");
     printArrayOfStrings(args, numArgs);
     printf("\n");
 }
 
-// void makeFullPath(char* path, char* fullPath){
-//     if (path[0] != '/'){
-//         if (path[0] == '.'){
-//             if (path[1] == '.' || path[1] == ' '){
-//                 fullPath = path;
-//             } else {
-//                 char* pathNoDot;
-//                 strcpy(pathNoDot, path + 1);
-//                 fullPath = strcat(getcwd(NULL, MAX_CWD_LEN),pathNoDot);
-//             }
-//         } else {
-//             fullPath = strcat(getcwd(NULL, MAX_CWD_LEN),path);
-//         }
-//     } else {
-//         fullPath = path;
-//     }
-// }
-
 void changeDirectories(char* path){
-    // char* fullPath;
-    // makeFullPath(path, fullPath);
     printf("Changing directory to %s\n", path);
     if (chdir(path) != 0){
-        printf("Failed to change directory to %s\n", path);
+        wrapError("chdir Failed:", strerror(errno));
     }
 }
 
-void executeCommand(char* command, char args [MAX_ARGS][MAX_ARG_LEN], int numArgs){
+void executeCommand(char args [MAX_TOKENS][MAX_TOKEN_LEN], int numArgs){
     char* newArgs[numArgs + 2];
-    newArgs[0] = command;
-    for (int i = 1; i < numArgs + 1; i++){
-        newArgs[i] = args[i - 1];
+    for (int i = 0; i < numArgs + 1; i++){
+        newArgs[i] = args[i];
     }
     newArgs[numArgs + 1] = NULL;
 
@@ -137,13 +119,23 @@ void executeCommand(char* command, char args [MAX_ARGS][MAX_ARG_LEN], int numArg
     }
     printf("\n");
     
+    int status;
     pid_t newPid = fork();
-    if (newPid == 0){
-        printf("Hello from child\n");
-        printf("executing command in child");
-        execvp(command, newArgs);
+    if (newPid == -1){
+        wrapError("fork Failed:", strerror(errno));
+    } else if (newPid == 0){
+        // Child Process
+        printf("executing command in child\n");
+        if(execvp(newArgs[0], newArgs) != 0){
+            wrapError("exec Failed:", strerror(errno));
+        }
+        exit;
     } else {
-        printf("Hello from Parent\n");
+        // Parent Process
+        pid_t childPid = wait(&status);
+        if (status != 0) {
+            printf("Child finished with error status: %d\n", status);
+        }
     }
 // use the fork system call and execvp system call
 // should launch a new child process and replace it with a running process with the system command
