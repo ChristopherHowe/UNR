f6457f0f18878b06c1722b341b04e0a9378774e4
ChristopherHowe <howechristopher5@gmail.com>
2024-03-02 17:46:09 -0800

    rewrote random number file generator to accept a command line val for the input

--- Spring-2024/446-Operating-Systems/PA2_Howe_Christopher/generate_random_ints.c
+++ Spring-2024/446-Operating-Systems/PA2_Howe_Christopher/generate_random_ints.c
@@ -2,19 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Check if the number of command line arguments is correct
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <num_of_numbers>\n", argv[0]);
+        return 1;
+    }
+
+    // Convert the command line argument to an integer
+    int num_of_numbers = atoi(argv[1]);
+
     // Set seed for random number generation
     srand(time(NULL));
 
+    // Create the file name based on the specified number of numbers
+    char file_name[50];
+    sprintf(file_name, "%d-random-numbers.txt", num_of_numbers);
+
     // Open file for writing
-    FILE *file = fopen("random_numbers.txt", "w");
+    FILE *file = fopen(file_name, "w");
     if (file == NULL) {
         fprintf(stderr, "Error opening file for writing.\n");
         return 1;
     }
 
-    // Generate and write 100,000,000 random integers to the file
-    for (int i = 0; i < 100000000; i++) {
+    // Generate and write the specified number of random integers to the file
+    for (int i = 0; i < num_of_numbers; i++) {
         int random_number = rand() % 100001; // Generate random number between 0 and 100,000
         fprintf(file, "%d\n", random_number);
     }
@@ -22,7 +35,7 @@ int main() {
     // Close the file
     fclose(file);
 
-    printf("Random numbers written to file successfully.\n");
+    printf("Random numbers written to file %s successfully.\n", file_name);
 
     return 0;
 }
