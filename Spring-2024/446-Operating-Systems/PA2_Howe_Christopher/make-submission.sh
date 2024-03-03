2f91ac9bf54dc336e22ef483cb4459f65ac9d2cc
ChristopherHowe <howechristopher5@gmail.com>
2024-03-02 17:09:29 -0800

    added code to create the assignment tarball

---
+++ Spring-2024/446-Operating-Systems/PA2_Howe_Christopher/make-submission.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+pa_name=PA2_Howe_Christopher
+
+mkdir $pa_name
+cd $pa_name
+mkdir loop
+mkdir thread
+cp ../loop/looped_sum.c ../loop/makefile ./loop
+cp ../thread/threaded_sum.c ../thread/makefile ./thread
+cd ..
+
+# Create the tarball
+tar -cvzf $pa_name.tar.gz $pa_name
+
+# delete the created folder
+rm -r $pa_name
+
+
+
