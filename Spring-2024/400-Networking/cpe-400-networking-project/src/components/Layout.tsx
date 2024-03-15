157a292538432b29f6901dc10ffea746956cf734
ChristopherHowe <howechristopher5@gmail.com>
2024-03-15 12:29:12 -0700

    updated layout to fit my needs

--- Spring-2024/400-Networking/cpe-400-networking-project/src/components/Layout.tsx
+++ Spring-2024/400-Networking/cpe-400-networking-project/src/components/Layout.tsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
-import {
-  Bars3Icon,
-  BellIcon,
-} from '@heroicons/react/24/outline'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import React, { ReactNode } from 'react';
 import Sidebar from './Sidebar';
 import MediumSidebarTransition from './SidebarTransition';
-import ProfileDropdown from './ProfileDropdown';
-import Searchbar from './Searchbar';
+import Seperator from './seperator';
 
 interface LayoutProps {
     children: ReactNode
@@ -30,22 +26,14 @@ export default function Layout(props: LayoutProps) {
             <span className="sr-only">Open sidebar</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
-
-          {/* Separator */}
-          <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
-
-          <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-            <Searchbar/>
-            <div className="flex items-center gap-x-4 lg:gap-x-6">
-              <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-
-              {/* Separator */}
-              <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
-
-              <ProfileDropdown/>
+          <Seperator/>
+          <h1 className="text-xl font-semibold">CPE 400 Networking Project</h1>
+          <div className='absolute right-0 p-10 text-right'>
+            <div className="text-md">
+            Created by Christopher Howe 
+            </div>
+            <div className="text-xs">
+            Styling from tailwind UI
             </div>
           </div>
         </div>
