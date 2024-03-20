608ac5a154aed5af329a6cbe981328a210ce1625
ChristopherHowe <howechristopher5@gmail.com>
2024-03-20 12:57:29 -0700

    Setting up project

---
+++ Spring-2024/485-Computer-Vision/Interest-Point-Project-2/make-submission.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+SUB_DIR=Project2
+ZIP_NAME=howe_christopher_project2.zip
+
+mkdir $SUB_DIR
+
+cp README.md ./$SUB_DIR/README.txt
+cp project2.py ./$SUB_DIR
+
+zip $ZIP_NAME $SUB_DIR
+
+
---
+++ Spring-2024/485-Computer-Vision/Interest-Point-Project-2/notes.md
@@ -0,0 +1,9 @@
+# Notes
+## Available libraries
+numpy, math, cv2, PIL, matplotlib, skimage
+
+## Deliverable
+single zip file (howe_christopher.zip)
+- project2.py
+- readme.md
+
