83af1751f3b647b5290512731d0e8495eec1238a
ChristopherHowe <howechristopher5@gmail.com>
2024-03-22 16:38:19 -0700

    finished specifying project docs

--- Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/project_requirements.md
+++ Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/project_requirements.md
@@ -5,8 +5,10 @@ stdio, stdlib, string, pthread, sys/wait.h, sys/types.h, unistd.h, fcntl.h, errn
 ## Requirements
 No global variables
 function prototypes are required
+use locking (Should not be optional via command line arguments)
+use method 2 of adjusting scheduling settings  (See content_notes.md)
 
 
 ## Goal
-Develop a program that creates a user-controlled number of threads htat will each be doing busy-wor and examine the comparative behavior of manipulating on of the threads scheduling properties and affinity.
-
+Develop a program that creates a user-controlled number of threads that will each be doing busy-wor and examine the comparative behavior of manipulating on of the threads scheduling properties and affinity.
+observe the maximum latency and its variation indirectly, through a timing that happens within the fast inner for loop and its maximum value over each while loop iteration
--- Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/sched.c
+++ Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/sched.c
@@ -10,7 +10,7 @@ Date: 3/1/24
 // #include <sys/time.h>
 #include <pthread.h>
 
-// ADD DEFINE STATEMENTS HERE
+// Macros
 
 // Thread Data structure (Provided in assignment specifications)
 typedef struct _thread_data_t {
@@ -23,7 +23,37 @@ typedef struct _thread_data_t {
 // Function Prototypes
 // Required
 void* arraySum(void* a);
+// Additional
+
+
 // Main Loop
 int main(int argc, char* argv[]){   
+// Check that 2 Command Line arguments are provided, otherwise log not enough parameters, return -1
+// Dynamically allocate arr of 2,000,000 ints
+// create long long int totalSum to hold sum of arr with val 0
+// create a mutex
+// Make an arr of len <cli Arg> of thread data
+    // vals
+        // localTid (thread ID) = should range from 0 - cliArg
+        // pointer to prev allocated arr
+        // numVals Corresponding to arr size
+        // lock field pointing to created mutex
+        // pointer to totalSum
+// Make an arr of len <cli arg> of threads that each call arraySum with the corresponding thread data.
+// join all the threads
+}
 
+void* arraySum(void* a){
+// This function should run indefinitely (inside while 1)
+    // every loop of the while loop should sum all the values of the array and incrmement the totalSum value by the sum
+    // Create a double storing the max latency
+    // For each iteration of the for loop
+        // add a val from the arr to the localSum
+        // calculate the latency of the for loop
+            // create a struct timespec object at the beggining of the loop using clock_gettime
+            // create a struct timespec object at the end of the loop using clock_gettime
+            // calculate the duration of the for loop iteration (ns) (long int)
+            // update the max latency
+    // For each iteration of the while loop extract the max latency for all the for loop iterations
+    // print the max latency with print_progress(pid_t local_tid, size_t value)
 }
