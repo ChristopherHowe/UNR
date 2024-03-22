d336c7e07831aa1e4fb0ec18ca08bd8297e80a3b
ChristopherHowe <howechristopher5@gmail.com>
2024-03-22 16:50:33 -0700

    updated submission generator

--- Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/make_submission.sh
+++ Spring-2024/446-Operating-Systems/PA3_Howe_Christopher/make_submission.sh
@@ -5,7 +5,9 @@ SUB_DIR=PA3_Howe_Christopher
 ZIP_DIR=$SUB_DIR.zip
 
 mkdir  $SUB_DIR
+
 cp makefile $SUB_DIR/Makefile
 cp sched.c $SUB_DIR
+cp ./latex-out/main.pdf ./$SUB_DIR/PA3_Howe_Christopher_Questions.pdf
 
 zip $ZIP_NAME $SUB_DIR
