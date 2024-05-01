180838645dbf99d7458776ad8d88b59bba1a6750
ChristopherHowe <howechristopher5@gmail.com>
2024-05-01 12:27:52 -0700

    styling

--- Spring-2024/400-Networking/cpe-400-networking-project/src/components/HostNode.tsx
+++ Spring-2024/400-Networking/cpe-400-networking-project/src/components/HostNode.tsx
@@ -27,13 +27,13 @@ export default function HostNode({ data }: NodeProps<NodeData>) {
     <>
       <Handle type="source" id={'top'} position={Position.Top} style={{ background: '#555' }} isConnectable />
       <Handle type="source" id={'bottom'} position={Position.Bottom} style={{ background: '#555' }} isConnectable />
-      <div className="border-2 bg-blue-500 border-black p-1 w-40 h-20 flex flex-col items-center justify-center rounded-md">
+      <div className="border-2 bg-blue-500 border-black p-2 w-40 flex flex-col items-center justify-center rounded-md">
         <div className="text-sm text-white">{host.name}</div>
         <div className="text-xs text-gray-300">{host.macAddress}</div>
         {host.ipAddress && <div className="text-xs text-gray-300">{host.ipAddress}</div>}
         <div className="flex flex-row items-center gap-3">
           <button onClick={() => setEditMac(host.macAddress)} className="text-gray-300 hover:text-gray-50">
-            Edit
+            Packets
           </button>
           {host.queuedPackets.length >= 1 ? (
             <div className="bg-yellow-300 border-yellow-400 border-2 rounded-full text-yellow-600 px-1 text-xs h-5">
